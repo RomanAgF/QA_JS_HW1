8ea81199082397b63c9f58a24a76ef6f56b14022
Roman Agadzhanyan <gt-tuning@bk.ru>
2022-04-20 09:47:08 +0300

    edit DZ

--- h2_1_2_2.js
+++ h2_1_2_2.js
@@ -4,19 +4,23 @@ let age_2 = 18; // Создать переменную “age_2” и присв
 let age_3 = 60; // Создать переменную “age_3” и присвоить ей значение 60
 
 function checkAge(age) {
-  if (typeof age === "number" && age < age_2) {
-    console.log(
-      "You don’t have access cause your age is " +
-        age +
-        " It’s less then " +
-        age_2
-    );
-  } else if (age >= age_2 && age < age_3) {
-    console.log("Welcome !");
-  } else if (age >= age_3) {
-    console.log("Keep calm and look Culture channel");
+  if (typeof age === "number") {
+    if (age < age_2) {
+      console.log(
+        "You don't have access cause your age is " +
+          age +
+          " It's less then " +
+          age_2
+      );
+    } else if (age >= age_2 && age == age_3) {
+      console.log("Welcome  !");
+    } else if (age >= age_3) {
+      console.log("Keep calm and look Culture channel");
+    } else {
+      console.log("Technical work");
+    }
   } else {
-    console.log("Technical work");
+    console.log("Неверный тип данных!");
   }
 }
 
